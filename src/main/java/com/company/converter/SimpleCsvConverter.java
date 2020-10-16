b7b530b380c6f0b7e02dcec311e1e98b36a5ac17
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-16 21:36:39 +0200

    Add CSVReader dependency + readFile() and convert()

--- src/main/java/com/company/converter/SimpleCsvConverter.java
+++ src/main/java/com/company/converter/SimpleCsvConverter.java
@@ -2,19 +2,33 @@ package com.company.converter;
 
 
 import com.company.OutputFormat;
+import com.company.dao.FileReader;
+import com.company.factory.OutputFormatterFactory;
+import com.company.view.OutputFormatter;
 
-import java.io.FileReader;
 import java.nio.file.Path;
 
 public class SimpleCsvConverter {
-    public void convert(Path file, OutputFormat outputFormat) {
 
+    private final FileReader fileReader;
+    private final OutputFormatterFactory outputFormatterFactory;
+
+    public SimpleCsvConverter(FileReader fileReader, OutputFormatterFactory outputFormatterFactory) {
+        this.fileReader = fileReader;
+        this.outputFormatterFactory = outputFormatterFactory;
     }
-//    private FileReader fileReader;
-//    private OutputFormatterFactory outputFormatterFactory;
-//
-//    public SimpleCsvConverter(FileReader fileReader, OutputFormatterFactory outputFormatterFactory) {
-//        this.fileReader = fileReader;
-//        this.outputFormatterFactory = outputFormatterFactory;
-//    }
+
+    public void convert(Path file) {
+        OutputFormat outputFormat = OutputFormat.TABLE;
+
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
+        System.out.printf("I convert CSV to %s%n", outputFormat);
+        outputFormatter.printToConsole(this.fileReader.readData(file));
+    }
+
+    public void convert(Path file, OutputFormat outputFormat) {
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
+        System.out.println(String.format("I convert CSV to %s", outputFormat));
+        outputFormatter.printToConsole(this.fileReader.readData(file));
+    }
 }
