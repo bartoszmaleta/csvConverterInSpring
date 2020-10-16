f94e2cdc738eeb779b0c6fc110adffd29e065be5
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-16 21:40:01 +0200

    Add fasterxml.jackson dependency

---
+++ src/main/java/com/company/view/JsonOutputFormatter.java
@@ -0,0 +1,16 @@
+package com.company.view;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
+
+public class JsonOutputFormatter {
+
+    public void printToConsole(List<Map<String, String>> data) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+//            String jsonResult = mapper.writ
+        }
+    }
+}
